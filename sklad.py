476b628d92e74513d6b77ff5b8edda7b98725954
ruman1990 <79218144+ruman1990@users.noreply.github.com>
2025-06-19 15:01:30 +0200

    Update sklad.py

--- sklad.py
+++ sklad.py
@@ -1,6 +1,55 @@
-produkty = ['voda','chlieb']
-pocty = [5,10]
-ceny = [2.20,3.50]
+# Pre studijne ucely, nepracujeme s decimalom pri cenach
+produkty = []
+pocty = []
+ceny = []
+
+def export_skladu():
+    if len(pocty) == 0:
+        print('Sklad je prazdny')
+        return
+    for x in range(len(produkty)):
+        print(f'{produkty[x]};{pocty[x]};{ceny[x]}|',end='')
+    print('')
+    print('Uspesne vyexportovane')
+
+def import_skladu(data):
+    global produkty, pocty, ceny
+    nove_produkty = []
+    nove_pocty = []
+    nove_ceny = []
+
+    polozky = data.strip().split("|")
+    for polozka in polozky:
+        casti = polozka.split(";")
+        if len(casti) != 3:
+            print("Chybný formát položky:", polozka)
+            continue
+        nazov = casti[0]
+        try:
+            pocet = int(casti[1])
+            cena = float(casti[2])
+        except ValueError:
+            print("Chybný počet alebo cena pri položke:", polozka)
+            continue
+
+        nove_produkty.append(nazov)
+        nove_pocty.append(pocet)
+        nove_ceny.append(cena)
+
+    # Nahradenie existujúceho skladu novými údajmi
+    produkty = nove_produkty
+    pocty = nove_pocty
+    ceny = nove_ceny
+    print("Sklad bol úspešne importovaný.")
+
+def nastav_cenu(pnazov):
+    if pnazov not in produkty:
+        nie_je_na_sklade(pnazov)
+    else:
+        cena = float(input('Zadaj cenu za kus: '))
+        index = produkty.index(pnazov)
+        ceny[index] = cena
+        print(f'Cena {cena} EUR uspesne nastavena pre produkt {pnazov}')
 
 def nie_je_na_sklade(pnazov):
     print(f'Produkt {pnazov} nie je vedeny na sklade')
@@ -50,7 +99,7 @@ def pridat_produkt(pnazov):
 
 def vypisat_produkty():
     for x in range(len(produkty)):
-        print(f'{produkty[x]} {pocty[x]}ks {ceny[x]}eur')
+        print(f'{produkty[x]} {pocty[x]}ks {ceny[x]} EUR')
 
 def zobraz_menu():
     print('\n========= MENU SKLADU =========')
@@ -60,6 +109,9 @@ def zobraz_menu():
     print('3. Odstranit produkt')
     print('4. Naskladnit produkt')
     print('5. Vyskladnit produkt')
+    print('6. Nastavit cenu')
+    print('7. Import skladu')
+    print('8. Export skladu')
     print('')
 
 
@@ -83,5 +135,14 @@ while True:
     elif volba == '5':
         nazov = input('Zadaj nazov produktu na vyskladnenie ')
         vyskladnit_produkt(nazov)
+    elif volba == '6':
+        nazov = input('Zadaj nazov produktu na nastavenie ceny ')
+        nastav_cenu(nazov)
+    elif volba == '7':
+        data = input('Zadaj data skladu: ')
+        import_skladu(data)
+    elif volba == '8':
+        export_skladu()
+        
 
 
