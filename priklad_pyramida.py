24f616cf7f042b7a707c54a4474318de1200fa7b
ruman1990 <79218144+ruman1990@users.noreply.github.com>
2025-07-09 14:47:36 +0200

    Create priklad_pyramida.py

---
+++ priklad_pyramida.py
@@ -0,0 +1,13 @@
+pocet = int(input('Kolko hviezdiciek: '))
+for x in range(1,pocet+1):
+    print('*' * x)
+for x in reversed(range(1,pocet)):
+    print('*' * x)
+
+#*
+#**
+#***
+#****
+#***
+#**
+#*
