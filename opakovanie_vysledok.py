1cdbd0522068752f29d6001d0cdadaeea17016e1
ruman1990 <79218144+ruman1990@users.noreply.github.com>
2025-07-10 09:19:47 +0200

    Create opakovanie_vysledok.py

---
+++ opakovanie_vysledok.py
@@ -0,0 +1,6 @@
+a = int(input("Zadaj prve cislo: "))
+b = int(input("Zadaj druhe cislo: "))
+if b == 0:
+    print('Delenie nulou nie je mozne')
+else:
+    print(f'Sucet: {a+b}, rozdiel: {a-b}, sucin: {a*b}, podiel: {a/b} ')
