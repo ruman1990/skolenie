ae5863c17eb56fe5a0e75b26ef95a09dcb9f07ae
ruman1990 <79218144+ruman1990@users.noreply.github.com>
2025-07-17 08:39:36 +0200

    Create opakovanie2.py

---
+++ pythonII/opakovanie2.py
@@ -0,0 +1,36 @@
+# Priklad 1: Slovník so študentmi
+# Zadanie:
+# Vytvor slovník, kde kľúčom bude meno študenta a hodnotou jeho známka
+# (napr. { 'Jana': 2, 'Peter': 1, ... }).
+# Pridaj do slovníka nového študenta a vypíš priemernú známku triedy.
+
+# ---------------------------------------------------------
+
+# Priklad 2: Zoznam slovníkov
+# Zadanie:
+# Vytvor zoznam, kde každý prvok je slovník s informáciami o knihe (nazov, autor, rok).
+# Pridaj 3 knihy a vypíš názvy všetkých kníh vydaných po roku 2010.
+
+
+# ---------------------------------------------------------
+
+# Priklad 3: Základná trieda Kniha (objekty)
+# Zadanie:
+# Vytvor triedu Kniha, ktorá má atribúty nazov, autor a rok.
+# Vytvor objekt a vypíš o ňom informácie.
+
+
+# ---------------------------------------------------------
+
+# Priklad 4: Dedičnosť (E-Kniha)
+# Zadanie:
+# Vytvor triedu EKniha, ktorá dedí z Kniha a má navyše atribut velkost_MB.
+# Prepis metódu vypis_info tak, aby vypísala aj veľkosť súboru.
+
+
+# ---------------------------------------------------------
+
+# Priklad 5: Dedičnosť a kontrola roku (Metóda)
+# Zadanie:
+# Vytvor triedu StaraKniha, ktorá dedí z Kniha a má metódu je_historicka(),
+# ktorá vráti True ak bola vydaná pred rokom 1950.
