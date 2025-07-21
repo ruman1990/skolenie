1c68095263adba3e77543e27d2dbf87a95961a12
ruman1990 <79218144+ruman1990@users.noreply.github.com>
2025-07-21 08:30:17 +0200

    Update opakovanie.py

--- pythonIII/opakovanie.py
+++ pythonIII/opakovanie.py
@@ -11,34 +11,3 @@
 
 # Výsledok zapíš do nového súboru vo formáte:
 # produkt, pocet_vyskladneni, posledny_datum_cas
-
-import re
-
-log_riadky = []
-with open("sklad/audit.log", encoding="utf-8") as f:
-    for line in f:
-        riadok = line.strip()
-        if riadok:
-            log_riadky.append(riadok)
-
-# Tento regex len vezme to, čo je v zátvorkách na začiatku a potom slovo za "Vyskladnenie "
-vzor = r"\[(.*?)\] - Vyskladnenie (.+)$"
-
-vyskladnenia = {}
-
-for riadok in log_riadky:
-    if "- Chyba:" not in riadok:
-        zhoda = re.match(vzor, riadok)
-        if zhoda:
-            datum = zhoda.group(1)
-            produkt = zhoda.group(2).strip()
-            if produkt not in vyskladnenia:
-                vyskladnenia[produkt] = []
-            vyskladnenia[produkt].append(datum)
-
-with open("vyskladnene_produkty.log", "w", encoding="utf-8") as fout:
-    fout.write("produkt,pocet_vyskladneni,posledny_datum_cas\n")
-    for produkt in vyskladnenia:
-        datumy = vyskladnenia[produkt]
-        fout.write(f"{produkt},{len(datumy)},{datumy[-1]}\n")
-
