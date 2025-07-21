0514e5727c6ad9cbbfb7a6e6c8bd0dcc6ee9adfb
ruman1990 <79218144+ruman1990@users.noreply.github.com>
2025-07-21 08:31:09 +0200

    Update opakovanie.py

--- pythonIII/opakovanie.py
+++ pythonIII/opakovanie.py
@@ -1,13 +1,13 @@
 
-# Načítaj celý log súbor do zoznamu.
+# Načítaj celý log súbor do zoznamu. (log.txt)
 
-# Pre každý riadok zisti, či ide o vyskladnenie produktu (úspešné aj neúspešné pokusy).
+# Pre každý riadok zisti, či ide o vyskladnenie produktu.
 
 # Vyfiltruj len tie riadky, kde bolo vyskladnenie úspešné (neobsahuje „Chyba“).
 
 # Pre každý produkt zisti, kedy (dátum a čas) bol úspešne vyskladnený.
 
-# Spočítaj, koľkokrát sa každý produkt vyskladňoval (úspešne alebo aj s chybou podľa zadania).
+# Spočítaj, koľkokrát sa každý produkt vyskladňoval.
 
 # Výsledok zapíš do nového súboru vo formáte:
 # produkt, pocet_vyskladneni, posledny_datum_cas
