94c44306df4cf61f8872da3ef1fd69495c35bd6f
Tvoje Meno <tvoj@email.com>
2025-07-29 08:35:49 +0200

    posledne opakovanie

---
+++ analytikI/opakovaniePosledne.py
@@ -0,0 +1,18 @@
+# letecka_preprava.py
+
+# 1. Vytvorenie letov pomocou namedtuple nazov Let a atributy cislo, odlet, ciel, pocet_pasazierov
+
+
+# 2. Prevod na dataclass, pridanie typu lietadla
+
+
+# 3. Uloženie do Excelu (openpyxl)
+
+
+# 4. Načítanie z Excelu
+
+
+# Prečítaj všetky riadky z Excelu a zapis do tablib Dataset a exportuj do CSV
+
+
+# 5. Sčítanie pasažierov z CSV cez tablib
