a0548c26be2e0d5e5281022ed5ddec9e8358ca51
Vladimir Ruman <rumanvladimir@gmail.com>
2025-08-25 18:03:28 +0200

    c

--- analytikI/priklad.py
+++ analytikI/priklad.py
@@ -1,43 +1,10 @@
 import pandas as pd
-import psycopg2
 
-conn = psycopg2.connect(
-    dbname='skolenie',
-    user='postgres',
-    password='postgres',
-    host='localhost',
-    port=5432
-)
+data = {
+    "vek": [20, 22, 23, 24, 25],
+    "vyska": [160, 165, 167, 170, 175],
+    "vaha": [55, 60, 61, 65, 70]
+}
+df = pd.DataFrame(data)
+print(df.loc[-1])
 
-data = pd.read_sql('select * from objednavka_view',conn)
-
-#print(data)
-
-najdrahsi = data.loc[data['cena'].idxmax()]
-
-najlacnejsi = data.loc[data['cena'].idxmin()]
-
-#print(najdrahsi)
-
-#print(najlacnejsi)
-
-#print(data['cena'],data['mnozstvo'])
-
-data['trzba'] = data['cena'] * data['mnozstvo']
-
-print(data['trzba'])
-
-obrat_podla_produktov = data.groupby('nazov')['trzba'].sum().reset_index()
-
-print(obrat_podla_produktov.head(20))
-
-import matplotlib.pyplot as plt
-
-
-plt.pie(obrat_podla_produktov['trzba'].head(10),labels=obrat_podla_produktov['nazov'].head(10),autopct='%1.1f%%')
-plt.show()
-
-
-
-#najdrahsi_obrat = obrat_podla_produktov.loc[obrat_podla_produktov['trzba'].idxmax()]
-#print(najdrahsi_obrat)
