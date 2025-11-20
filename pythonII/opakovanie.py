436c3555ddd125a3657e5f17dd4f89372e78b28f
Vladimir Ruman <rumanvladimir@gmail.com>
2025-11-20 18:20:46 +0100

    pythonII

--- pythonII/opakovanie.py
+++ pythonII/opakovanie.py
@@ -5,12 +5,6 @@
 # # Napíš funkciu, ktorá prijme reťazec a vráti ho v opačnom poradí (napr. "python" → "nohtyp").
 
 #x = "Python"
-# x = input("Zadaj string: ")
-
-# def otoc_string(vstup):
-#     return vstup[::-1]
-
-# print(otoc_string(x))
 
 
 
@@ -18,32 +12,17 @@
 # # Napíš program, ktorý vypíše všetky párne čísla od 1 do 50 na jeden riadok, oddelené medzerou.
 # # (Príklad: výstup: 2 4 6 ... 50)
 
-# for x in range(2,51,2):
-#     print(x,end=" ")
 
 # 3. Podmienený príkaz – priestupný rok
 # Napíš funkciu, ktorá zistí, či je zadaný rok priestupný.
 # # priestupny je ked: delitelny 4 a zaroven nedelitelny 100, vynimka je delitelnost 400
 # rok = 2024
 
-# def je_priestupny(r):
-#     if r % 4 == 0 and (r % 100 != 0 or r % 400 == 0):
-#         return True
-#     else:
-#         return False
-    
-# print(je_priestupny(rok))
 
 # 4. Funkcia – najväčšie číslo zo zoznamu
 # Napíš funkciu, ktorá prijme zoznam čísel a vráti najväčšie číslo v zozname.
 # y = [9,1,5,3,7,25,2]
 
-# def najvacsie_cislo(zoznam):
-#     naj = zoznam[0]
-#     for x in zoznam:
-#         if x > naj:
-#             naj = x
-#     return naj
 
 # print(najvacsie_cislo(y))
 
@@ -56,25 +35,17 @@
 # Z reťazca vypíš len prvých 5 znakov a posledné 3 znaky.
 # slovo = "programovanie"
 
-# prve = slovo[:5]
-# posledne = slovo[-3:]
-
-# print(f'{prve} {posledne}')
 
 # 7. Pridávanie a odoberanie prvkov zo zoznamu
 # Vytvor zoznam s tromi ovocnými názvami. Pridaj do zoznamu ďalšie ovocie, potom jedno ovocie zo zoznamu vymaž a vypíš výsledný zoznam.
 # ovocie = ["jablko", "banán", "hruška"]
-# print(ovocie)
-# ovocie.append("broskyna")
-# print(ovocie)
 
-# ovocie.remove("jablko")
-# print(ovocie)
+
 # 8. Zložitejšie vyrezávanie stringov
 # Funkcia, ktorá vráti reťazec s vynechanými znakmi na párnych indexoch.
 # retazec = "programovanie"
 
-# print(retazec[1::2])
+
 
 # 9. Práca so zoznamom – vkladanie, odoberanie a vyhľadávanie
 # 1. Vytvor zoznam čísel od 1 do 10.
@@ -85,7 +56,7 @@
 # 10. Funkcia – práca so zoznamom mien
 # Funkcia, ktorá zo zoznamu mien vyreže z každého mena prvé písmeno.
 mena = ["Martin", "Eva", "Jozef"]
-print(mena[0][1:])
+
 # # Výsledok: ["artin", "va", "ozef"]
 
 # 11. Odstránenie záporných čísel zo zoznamu
@@ -98,19 +69,19 @@ cisla = [5, -3, 8, -1, 0, 2, -7, 6]
 # Odstráň medzery na začiatku a na konci reťazca.
 # vetas_medzerami = "   Python je super!   "
 # print(vetas_medzerami)
-# print(vetas_medzerami.strip())
+
 
 
 # 13. Použitie isnumeric()
 # Skontroluj, či reťazec obsahuje iba číslice.
 vstup = "12345"
-print(vstup.isnumeric())
+
 vstup = "12ab45"
-print(vstup.isnumeric())
+
 
 # 14. Použitie title()
 # Zmena mena a priezviska na formát s veľkými začiatočnými písmenami.
 cele_meno = "jan novak"
 # # Výstup: "Jan Novak"
-print(cele_meno.title())
+
 
--- pythonII/opakovanie2.py
+++ pythonII/opakovanie2.py
@@ -5,18 +5,8 @@
 # Pridaj do slovníka nového študenta a vypíš priemernú známku triedy.
 znamky = {'Jana' : 2, 'Peter' : 1, 'Marek' : 3, 'Lucia' : 2}
 
-znamky['Vlado'] = 1 
-#znamky.update({'Vlado' : 1})
 
 #[2,1,3,2]
-priemer = sum(znamky.values()) / len(znamky)
-
-suma = 0
-for x in znamky.values():
-    suma += x
-priemer = suma / len(znamky)
-
-print(f'Priemerna znamka triedy je {priemer}')
 
 
 # ---------------------------------------------------------
@@ -31,11 +21,6 @@ knihy = [
     {'nazov': 'Sila zvyku', 'autor': 'Charles Duhigg', 'rok': 2012}
 ]
 
-for x in knihy:
-    if x['rok'] > 1960:
-        print(f'{x['nazov']} {x['autor']}')
-
-
 # ---------------------------------------------------------
 
 # Priklad 3: Základná trieda Kniha (objekty)
@@ -44,24 +29,6 @@ for x in knihy:
 # Vytvor objekt a vypíš o ňom informácie.
 
 
-class Kniha:
-
-    def __init__(self,nazov, autor, rok):
-        self.nazov = nazov
-        self.autor = autor
-        self.rok = rok
-
-    def __str__(self):
-        return f'{self.nazov} {self.autor} {self.rok}'
-    
-    def __repr__(self):
-        return f'{self.nazov} {self.autor} {self.rok}'
-
-knihyObjekty = []
-for x in knihy:
-    knihyObjekty.append(Kniha(x['nazov'],x['autor'],x['rok']))
-
-print(knihyObjekty)
 
 # ---------------------------------------------------------
 
@@ -71,22 +38,6 @@ print(knihyObjekty)
 # Prepis metódu vypis_info tak, aby vypísala aj veľkosť súboru.
 
 y = ['nazov knihy','autor knihy',2000,'56464654']
-
-class EKniha(Kniha):
-
-    def __init__(self,nazov,autor,rok,velkost_MB=10):
-        super().__init__(nazov,autor,rok)
-        self.velkost_MB = velkost_MB
-
-    def __str__(self):
-        return f'{super().__str__()} {self.velkost_MB}'
-    
-    def vypis_info(self):
-        print(f'{super().__str__()} {self.velkost_MB}')
-
-ekniha = EKniha('maly princ','Saint Exupery',1943,25)
-
-ekniha.vypis_info()
 
 
 
@@ -98,18 +49,3 @@ ekniha.vypis_info()
 # ktorá vráti True ak bola vydaná pred rokom 1950.
 
 
-class StaraKniha(Kniha):
-
-    def je_historicka(self):
-        return self.rok < 1950
-    
-    @classmethod
-    def from_book(cls,book):
-        return cls(book.nazov,book.autor,book.rok)
-
-book = Kniha('1984','George Orwell',1948)
-oldBook = StaraKniha('1984','George Orwell',1948)
-
-print(oldBook.je_historicka())
-book = StaraKniha.from_book(book)
-print(book.je_historicka())
