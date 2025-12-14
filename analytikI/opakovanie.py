45034fec9164a39dd564cc309cdd5e5088fd66c8
Vladimir Ruman <rumanvladimir@gmail.com>
2025-12-14 22:30:39 +0100

    analytikI

--- analytikI/opakovanie.py
+++ analytikI/opakovanie.py
@@ -3,33 +3,10 @@
 # # 1. Nájdite viaceré spôsoby, ako vypísať "hello there" 7-krát.
 # #    (Použite aspoň for cyklus, while cyklus, rekurziu, list comprehension alebo iný spôsob.)
 
-# # DOPLŇTE KÓD TU
-# for _ in range(7):
-#     print("hello there", end=' ')
 
-# print()
-# print('hello there ' * 7)
-
-# print(*['hello there' for _ in range(7)],end=' ')
-# print()
-# def print_hello(n):
-#     if n <= 0:
-#         return
-#     print("hello there", end=' ')
-#     print_hello(n - 1)
-
-# print_hello(7)
-# print()
 # # 2. Vytvorte z premennej vals n-ticu (tuple) unikátnych hodnôt.
 
 # vals = [1, 1, 1, 2, 2, 3, 3, 4, 5, 5, 5, 5, 6, 7, 8, 9, 10]
-
-# x = tuple(set(vals))
-# print(x)
-
-# import funcy
-# print(tuple(funcy.distinct(vals)))
-
 
 # # # DOPLŇTE KÓD TU
 
@@ -40,37 +17,13 @@
 #      'small', 'terrific', 'alternative', 'book', 'dictionary', 'word'
 #  ]
 
-# for x in words:
-#     if 'r' in x:
-#         print(x)
 
-# print()
-# x = [ word for word in words if 'r' in word]
-# print(*x)
-
-# print()
-# for x in filter(lambda x : 'r' in x,words):
-#     print(x, end=' ')
-
-
-# print()
-# print()
 # # DOPLŇTE KÓD TU
 
 # # 4. Stiahnite JSON zo stránky a vypíšte všetky emaily používateľov.
 # #    URL: https://webcode.me/users.json
 
 # # DOPLŇTE KÓD TU
-# import requests
-
-# x = requests.get('https://webcode.me/users.json')
-# data = x.json()
-
-# for x in data['users']:
-#     print(x['email'])
-
-# print()
-# print(*[x['email'] for x in data['users']])
 
 
 # 5. Práca so zoznamom miest:
@@ -98,32 +51,13 @@ cities = [
 # Úlohy:
 # a) Vypíšte prvých 5 miest.
 
-for x in range(5):
-    print(cities[x]['name'])
-
-for x in cities[:5]:
-    print(x['name'])
-
-print(*[ x['name'] for x in cities[:5] ])
 
 # b) Vypíšte posledných 5 miest.
-for x in cities[-5:]:
-    print(x['name'])
 
-print()
 # c) Nájdite mesto s najväčším počtom obyvateľov.
 
-print(max(cities,key=lambda x : x['population'] ))
-
-
 # d) Nájdite mesto s najmenším počtom obyvateľov.
-
-print(min(cities,key=lambda x : x['population'] ))
 
 # e) Vyfiltrujte mestá, ktoré majú menej ako 1 milión obyvateľov.
 
 # DOPLŇTE KÓD TU
-print()
-print(*[c['name'] for c in cities if c['population'] < 1_000_000])
-
-print(list(filter(lambda x: x['population'] < 1_000_000,cities)))
--- analytikI/opakovaniePosledne.py
+++ analytikI/opakovaniePosledne.py
@@ -8,102 +8,22 @@
 
 
 # 1. Vytvorenie letov pomocou namedtuple nazov Let a atributy cislo, odlet, ciel, pocet_pasazierov
-from collections import namedtuple
 
-Let = namedtuple('Let',['cislo','odlet','ciel','pocet_pasazierov'])
-
-zoznam = [
-    Let('LH123','Viedeň','Berlín',150),
-    Let('BA456','Londýn','Paríž',200),
-    Let('AF789','Praha','Rím',180),
-    Let('KL101','Amsterdam','Madrid',220),
-    Let('LX202','Ženeva','Brusel',170)
-]
 
 # 2. Prevod na dataclass, pridanie typu lietadla
 
-from dataclasses import dataclass
-
-@dataclass
-class CLet:
-    cislo: str
-    odlet: str
-    ciel: str
-    pocet_pasazierov: int
-
-    def __repr__(self):
-        return f'{self.cislo},{self.odlet},{self.ciel},{self.pocet_pasazierov}'
-
-# dataclass_zoznam = []
-# for x in zoznam:
-#     dataclass_zoznam.append(CLet(x.cislo,x.odlet,x.ciel,x.pocet_pasazierov))
-
-dataclass_zoznam = [CLet(x.cislo,x.odlet,x.ciel,x.pocet_pasazierov) for x in zoznam]
-
-print(dataclass_zoznam)
 
 # 3. Uloženie do Excelu (openpyxl)
-from openpyxl import Workbook
 
-wb = Workbook()
-ws = wb.active
-
-ws.append(['cislo', 'odlet', 'ciel', 'pocet_pasazierov'])
-
-for x in dataclass_zoznam:
-    ws.append([x.cislo,x.odlet,x.ciel,x.pocet_pasazierov])
-
-wb.save('lety.xlsx')
 
 # precitaj vsetky riadky a zapis ich do postgre databazy, predtym vytvo tabulku letov
 # 4. Uloženie do Postgre (psycopg2)
 
-import psycopg2
-
-with psycopg2.connect('dbname=skolenie user=postgres password=admin host=localhost') as conn:
-    with conn.cursor() as cur:
-
-        cur.execute('''
-                CREATE TABLE IF NOT EXISTS lety (
-                        id SERIAL PRIMARY KEY,
-                        cislo TEXT,
-                        odlet TEXT,
-                        ciel TEXT,
-                        pocet_pasazierov INTEGER
-                    )
-                    ''')
-        
-        cur.execute('DELETE FROM lety')
-
-        for a in dataclass_zoznam:
-            cur.execute('INSERT INTO lety (cislo,odlet,ciel,pocet_pasazierov) VALUES (%s,%s,%s,%s)',
-                        (a.cislo,a.odlet,a.ciel,a.pocet_pasazierov))
-
-        conn.commit()
-
 
 # 5. spocitaj pocet pasazierov celkovo pomocou sql a cez python
 
-        cur.execute('select sum(pocet_pasazierov) from lety')
-        result = cur.fetchone()
-        print(f'Celkovy pocet pasazierov je {result[0]}')
-
-        result = sum([a.pocet_pasazierov for a in dataclass_zoznam])
-        print(f'Celkovy pocet pasazierov je {result}')
-
+ 
 #6. vypis vsetky lety do Pariza a kolko ich je cez sql a python
-        cur.execute('select * from lety where ciel=%s',('Paríž',))
-
-        result = cur.fetchall()
-        print(f'Vsetky lety do Pariza {result}',f'Pocet {len(result)}')
-
-        result = [a for a in dataclass_zoznam if a.ciel == 'Paríž']
-        print(f'Vsetky lety do Pariza {result}',f'Pocet {len(result)}')
-
+ 
 #7. let s najvacsim poctom pasazierov v sql a python
-        cur.execute('select * from lety order by pocet_pasazierov desc limit 1')
-        result = cur.fetchone()
-        print(f'Najvacsi pocet pasazierov je {result}')
-
-        result = max(dataclass_zoznam,key= lambda x : x.pocet_pasazierov)
-        print(f'Najvacsi pocet pasazierov je {result}')
+ 
