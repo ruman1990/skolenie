9dde24b8827cf07fa917002a7eef1019627ff281
Vlado <email@email.com>
2026-01-30 15:08:07 +0100

    analytikI

--- analytikI/priklad.py
+++ analytikI/priklad.py
@@ -1,14 +1,37 @@
+STOP = {"TAA", "TAG", "TGA"}
+E = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi"
+acc = "NC_000913.3"  # E. coli K-12 MG1655 complete genome
+params={"db":"nuccore","id":acc,"rettype":"fasta","retmode":"text"}
+
+import requests
+
+data = requests.get(E,params=params)
+with open('Ecoli.txt','w',encoding='utf-8') as f:
+    f.write(data.text)
+
+seq = []
+with open('Ecoli.txt','r',encoding='utf-8') as f:
+    for line in f:
+        if '>' in line:
+            continue
+        seq.append(line.strip())
+
+import re
+parts = re.split("|".join(STOP),data.text)
+print(parts)
+
+row = []
+
+for x in parts:
+    d = {}
+    d['start'] = len(x)
+    d['sekvencia'] = x
+    d['pocetG'] = x.count('G')
+    row.append(d)
+
 import pandas as pd
-import psycopg2
+df = pd.DataFrame(row)
+print(df)
 
-conn = psycopg2.connect(
-    database = 'postgres',
-    host = 'localhost',
-    user = 'postgres',
-    password = 'admin'
-)
 
-df = pd.read_csv('users3.csv')
-df.loc[df["salary"] < 0,'salary'] = 0
-print(df[df['salary'] < 0])
-df.to_json('vystup3.json',orient="records",force_ascii=False,indent=4)
+    
