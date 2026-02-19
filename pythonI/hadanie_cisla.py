517bdd9f1f48a227ed70221f69bbcae184df8d29
ruman1990 <79218144+ruman1990@users.noreply.github.com>
2026-02-19 14:26:39 +0100

    Update hadanie_cisla.py

--- pythonI/hadanie_cisla.py
+++ pythonI/hadanie_cisla.py
@@ -1,21 +1,18 @@
 import random
 
-secret_number = random.randint(1,100)
-
-pocet_pokusov = 0
-
-while pocet_pokusov < 7: 
-    cislo = int(input(f'Hadaj cislo (Zostavajuce pokusy {7 - pocet_pokusov}): '))
-    if cislo == secret_number:
-        print('Vyhral si')
-        break
-    elif cislo < secret_number:
-        print('Vacsie')
+secret_number = random.randint(0,100)
+pocet_pokusov = 6
+while pocet_pokusov > 0:
+    guess_number = int(input(f"Hadaj cislo (0-100), zostava {pocet_pokusov} pokusov: "))
+    
+    if guess_number < secret_number:
+        print('too low')
+    elif guess_number > secret_number:
+        print('too high')
     else:
-        print('Mensie')
-    pocet_pokusov += 1
-
-if pocet_pokusov == 7:
-    print('Prehral si!')
-
+        print('You won')
+        break
+    pocet_pokusov -= 1
+if pocet_pokusov == 0:
+    print('You failed. Play again.')
 
