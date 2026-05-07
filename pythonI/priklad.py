f84e5f439cf14d19d858687bbb5442a6c416da2b
Tvoje Meno <tvoj@email.com>
2026-05-07 15:00:25 +0200

    pyhtonI

--- pythonI/priklad.py
+++ pythonI/priklad.py
@@ -1,20 +1,7 @@
-import random
+zoznam = [0,1,2,3]
 
-vyherne_cislo = random.randint(0,100)
-number = int(input("Hadaj cislo 0-100: "))
-pocet_pokusov = 6
-while number != vyherne_cislo:
-    pocet_pokusov -= 1
-    if pocet_pokusov == 0:
-        break
-    if number < vyherne_cislo:
-        print("Hadaj vyssie!")
-    else:
-        print("Hadaj nizsie!")
+zoznam.append(100)
+zoznam.insert(0,200)
 
-    number = int(input(f"Hadaj cislo 0-100 (Zostavajucich pokusov {pocet_pokusov}): "))
-
-if pocet_pokusov == 0:
-    print("Prehral si!")
-else:
-    print("Vyhral si!")
+zoznam.remove(3)
+print(zoznam)
--- pythonI/sklad.py
+++ pythonI/sklad.py
@@ -10,3 +10,32 @@
     # odstranenie tovaru zo skladu
     # exportovat sklad
     # importovat sklad
+from decimal import Decimal
+produkty = [['voda',1.5,10],['chlieb',2,20],['jogurt',0.5,10]]
+
+def pridanie_tovaru():
+    nazov = input("Zadaj nazov tovaru: ")
+    for x in produkty:
+        if nazov == x[0]:
+            print("Tovar uz existuje")
+            return
+    cena = Decimal(input("Zadaj cenu tovaru: "))
+    pocet_kusov = int(input("Zadaj pocet kusov tovaru: "))
+    produkty.append([nazov,cena,pocet_kusov])
+    print("Tovar bol uspesne pridany.")
+
+while True:
+
+    print("-----MENU-----")
+    print("1. Vypis skladu")
+    print("2. Pridanie tovaru na sklad")
+    print("0. Ukoncenie programu")
+
+    volba = input("Zadaj svoju volbu: ")
+    if volba == '0':
+        break
+    elif volba == '1':
+        for x in produkty:
+            print(f"Produkt {x[0]}, cena {x[1]:.2f}€, pocet {x[2]}")
+    elif volba == '2':
+        pridanie_tovaru()
