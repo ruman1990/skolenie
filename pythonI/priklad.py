2a736a7d54c529d522a59a92acbd93bd873b6089
Vladimir Ruman <rumanvladimir@gmail.com>
2026-05-18 14:52:49 +0200

    pythonI

--- pythonI/priklad.py
+++ pythonI/priklad.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import random
 
 secret_number = random.randint(1,100)
@@ -19,12 +18,3 @@ while True:
         print("Hadaj vyssie")
     
 
-=======
-zoznam = [0,1,2,3]
-
-zoznam.append(100)
-zoznam.insert(0,200)
-
-zoznam.remove(3)
-print(zoznam)
->>>>>>> 36bb11240ea46d7461a66da311d6e39e3fc7a012
