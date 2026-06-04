8011ecffd8f938c115897d01bcf88430af8db1bf
Tvoje Meno <tvoj@email.com>
2026-06-04 15:01:19 +0200

    pythonIII

---
+++ pythonIII/priklad.py
@@ -0,0 +1,8 @@
+import locale
+
+# Nastavenie slovenského locale (pre Unix/Linux)
+locale.setlocale(locale.LC_ALL, 'sk_SK.UTF-8')
+
+# Formátovanie čísla s oddelovačom tisícov
+cislo = 1234567.89
+print(locale.format_string("%.2f", cislo, grouping=True))  # napr. '1 234 567,89'
--- pythonIII/priklady.py
+++ pythonIII/priklady.py
@@ -1,15 +1,5 @@
-from funcy import lmap, lfilter, partial, compose
+import requests
 
-print(lmap(lambda x: x + 1, [1, 2, 3]))  # [2, 3, 4]
-print(lfilter(lambda x: x % 2 == 0, range(10)))  # [0, 2, 4, 6, 8]
+res = requests.post("http://127.0.0.1:5000/echo",json={ "moje_data" : 1111111111})
 
-
-def nasobok_dvoch(x):
-    return x * 2
-
-# Zreťazenie funkcií (compose)
-f = compose(str, nasobok_dvoch,abs)
-print(f(-15))  # '15'
-
-
-print(str(nasobok_dvoch(abs(-15))))
+print(res.json())
