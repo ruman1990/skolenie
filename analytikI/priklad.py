5816303b767fcbe6864ce12a1ebd0d0a74d535c6
Tvoje Meno <tvoj@email.com>
2026-07-08 13:41:50 +0200

    analytikI

--- analytikI/priklad.py
+++ analytikI/priklad.py
@@ -1,6 +1,39 @@
-import numpy as np
+from openpyxl import Workbook
+from openpyxl.chart import BarChart, Reference
+import csv
 
-arr = np.array([[1, 2, 3], [4, 5, 6]])
+with open("data.csv","r",encoding="utf-8") as f:
+    data = csv.reader(f)
 
-for x in np.nditer(arr):
-    print(x)
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Zamestnanci"
+
+    y = []
+    for x in data:
+        y.append(x)
+
+    z = [[int(x[0]),x[1],x[2],int(float(x[3]))] for x in y[1:]]
+
+    print(z)
+
+    ws.append(y[0])
+    for x in z:
+        ws.append(x)
+
+    ws.append(["priemer","=AVERAGE(D2:D6)"])
+
+    graf = BarChart()
+    graf.title = "Plat zamestnancov"
+
+    graf.y_axis.title = "Plat"
+    graf.x_axis.title = "Zamestnanec"
+
+    data = Reference(ws,min_col=4,min_row=2, max_row=6)
+    cat = Reference(ws,min_col=3,min_row=2,max_row=6)
+
+    graf.add_data(data)
+    graf.set_categories(cat)
+
+    ws.add_chart(graf,"F2")
+    wb.save("platy.xlsx")
--- analytikI/priklad_excel.py
+++ analytikI/priklad_excel.py
@@ -1,60 +1,39 @@
 from openpyxl import Workbook
 from openpyxl.chart import BarChart, Reference
-
-wb = Workbook()
-ws = wb.active
-ws.title = "Zamestnanci"
-
 import csv
+
 with open("data.csv","r",encoding="utf-8") as f:
-    reader = csv.reader(f)
-    ws.append(["Priezvisko","Plat"])
-    for x in list(reader)[1:]:
-        ws.append([x[2],int(float(x[3]))])
+    data = csv.reader(f)
 
-graf = BarChart()
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Zamestnanci"
 
-graf.title = "Platy zamestnancov v roku 2026"
+    y = []
+    for x in data:
+        y.append(x)
 
-graf.y_axis.title = "Plat"
-graf.x_axis.title = "Zamestnanec"
+    z = [[int(x[0]),x[1],x[2],int(float(x[3]))] for x in y[1:]]
 
-data = Reference(ws,min_col=2,min_row=2,max_row=6)
-kategories = Reference(ws,min_col=1,min_row=2,max_row=6)
+    print(z)
 
-graf.add_data(data)
-graf.set_categories(kategories)
-ws.add_chart(graf)
-wb.save("platy.xlsx")
+    ws.append(y[0])
+    for x in z:
+        ws.append(x)
 
+    ws.append(["priemer","=AVERAGE(D2:D6)"])
 
+    graf = BarChart()
+    graf.title = "Plat zamestnancov"
 
-# # 1. Dáta
-# ws.append(["Produkt", "Cena", "Kusov", "Spolu (€)"])
-# produkty = [
-#     ["Chleba", 1.5, 10],
-#     ["Mlieko", 0.9, 20],
-#     ["Maslo", 2.3, 5],
-# ]
+    graf.y_axis.title = "Plat"
+    graf.x_axis.title = "Zamestnanec"
 
-# # 2. Zápis dát a výpočet spolu
-# for produkt in produkty:
-#     cena, kusy = produkt[1], produkt[2]
-#     produkt.append(cena * kusy)
-#     ws.append(produkt)
+    data = Reference(ws,min_col=4,min_row=2, max_row=6)
+    cat = Reference(ws,min_col=3,min_row=2,max_row=6)
 
+    graf.add_data(data)
+    graf.set_categories(cat)
 
-# graf = BarChart()
-# graf.title = "Tržby podľa produktu"
-# graf.y_axis.title = "€"
-# graf.x_axis.title = "Produkt"
-
-# data = Reference(ws, min_col=4, min_row=1, max_row=4)
-# kategorie = Reference(ws, min_col=1, min_row=2, max_row=4)
-
-# graf.add_data(data, titles_from_data=True)
-# graf.set_categories(kategorie)
-
-# ws.add_chart(graf, "O2")
-
-# wb.save("priklad.xlsx")
+    ws.add_chart(graf,"F2")
+    wb.save("platy.xlsx")
