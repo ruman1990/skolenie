eb00818f153a6337cc2728995a67f0feac572a57
Tvoje Meno <tvoj@email.com>
2026-07-09 10:13:24 +0200

    analytikI

--- analytikI/priklad.py
+++ analytikI/priklad.py
@@ -1,39 +1,21 @@
-from openpyxl import Workbook
-from openpyxl.chart import BarChart, Reference
-import csv
+import psycopg2
 
-with open("data.csv","r",encoding="utf-8") as f:
-    data = csv.reader(f)
+conn = psycopg2.connect(
+    host="localhost",
+    database="test",
+    user="postgres",
+    password="postgres"
+)
 
-    wb = Workbook()
-    ws = wb.active
-    ws.title = "Zamestnanci"
+cur = conn.cursor()
 
-    y = []
-    for x in data:
-        y.append(x)
+cur.execute("select * from objednavky where typ='odberatelska' and datum <= '2024-03-31' and datum >= '2024-03-01'")
 
-    z = [[int(x[0]),x[1],x[2],int(float(x[3]))] for x in y[1:]]
 
-    print(z)
 
-    ws.append(y[0])
-    for x in z:
-        ws.append(x)
+res = cur.fetchall()
 
-    ws.append(["priemer","=AVERAGE(D2:D6)"])
+v = [x for x in res if x[4]==23]
 
-    graf = BarChart()
-    graf.title = "Plat zamestnancov"
-
-    graf.y_axis.title = "Plat"
-    graf.x_axis.title = "Zamestnanec"
-
-    data = Reference(ws,min_col=4,min_row=2, max_row=6)
-    cat = Reference(ws,min_col=3,min_row=2,max_row=6)
-
-    graf.add_data(data)
-    graf.set_categories(cat)
-
-    ws.add_chart(graf,"F2")
-    wb.save("platy.xlsx")
+for x in v:
+    print(v)
