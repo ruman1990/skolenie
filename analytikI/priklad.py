bcfb742544f038f19f5a67204b51e5fa45370b78
Tvoje Meno <tvoj@email.com>
2026-07-15 08:46:32 +0200

    analztikI

--- analytikI/priklad.py
+++ analytikI/priklad.py
@@ -1,21 +1,7 @@
-import psycopg2
+import matplotlib.pyplot as plt
+import numpy as np
 
-conn = psycopg2.connect(
-    host="localhost",
-    database="test",
-    user="postgres",
-    password="postgres"
-)
+y = np.array([35, 25, 25, 15])
 
-cur = conn.cursor()
-
-cur.execute("select * from objednavky where typ='odberatelska' and datum <= '2024-03-31' and datum >= '2024-03-01'")
-
-
-
-res = cur.fetchall()
-
-v = [x for x in res if x[4]==23]
-
-for x in v:
-    print(v)
+plt.pie(y)
+plt.show()
