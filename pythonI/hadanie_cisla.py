75c91ab861040f9a3e08cfcf27e7e3a6dc39602e
Tvoje Meno <tvoj@email.com>
2026-07-16 14:37:58 +0200

    pythonI

--- pythonI/hadanie_cisla.py
+++ pythonI/hadanie_cisla.py
@@ -1,10 +1,19 @@
 # pouzivatel nech hada tajne cislo a ked uhadne, tak vyhral inak prehral
 import random
 
+def get_pokusy_format(pokusy):
+    match pokusy:
+        case 7 | 6 | 5:
+            return f"{pokusy} pokusov"
+        case 4 | 3 | 2:
+            return f"{pokusy} pokusy"
+        case 1:
+            return f"{pokusy} pokus"
+
 secret_number = random.randint(1,100)
 pocet_pokusov = 7
 while(pocet_pokusov > 0):
-    number = int(input(f"Hadaj cislo od 1 po 100 (mas {pocet_pokusov} pokusov): "))
+    number = int(input(f"Hadaj cislo od 1 po 100 (mas {get_pokusy_format(pocet_pokusov)}): "))
     pocet_pokusov -= 1
     if number == secret_number:
         print("Uhadol si!")
