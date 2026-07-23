19178e4ad9184b501085589d743cc0917aea0c8c
Tvoje Meno <tvoj@email.sk>
2026-07-23 09:36:57 +0200

    pythonII

--- pythonII/opakovanie2.py
+++ pythonII/opakovanie2.py
@@ -4,6 +4,10 @@
 # (napr. { 'Jana': 2, 'Peter': 1, ... }).
 # Pridaj do slovníka nového študenta a vypíš priemernú známku triedy.
 
+studenti = { 'Jana' : 2 , 'Peter' : 1 , 'Zuzana' : 3 , 'Vlado' : 4}
+studenti['Adam'] = 1
+print(f"Priemerna znamka triedy je {sum(studenti.values())/len(studenti)}")
+
 
 
 # ---------------------------------------------------------
@@ -17,12 +21,36 @@ x = [{"nazov" : "python guru I", "autor" : "Guru" , "rok" : 2008},
      {"nazov" : "python guru III", "autor" : "Guru" , "rok" : 2017}]
 
 
+x.append({"nazov" : "python guru IV", "autor" : "Guru" , "rok" : 2020})
+
+result = []
+for i in x:
+    if i['rok'] > 2010:
+        result.append(i)
+
+print(result)
+
 # ---------------------------------------------------------
 
 # Priklad 3: Základná trieda Kniha (objekty)
 # Zadanie:
 # Vytvor triedu Kniha, ktorá má atribúty nazov, autor a rok.
 # Vytvor objekt a vypíš o ňom informácie.
+
+class Kniha:
+     def __init__(self,nazov,autor,rok):
+        self.nazov = nazov
+        self.autor = autor
+        self.rok = rok
+
+     def __str__(self):
+        return f"{self.nazov}, {self.autor}, {self.rok}"
+
+k = Kniha("Python I","Guru",1995)
+
+print(k)
+
+
 
 # ---------------------------------------------------------
 
@@ -31,6 +59,23 @@ x = [{"nazov" : "python guru I", "autor" : "Guru" , "rok" : 2008},
 # Vytvor triedu EKniha, ktorá dedí z Kniha a má navyše atribut velkost_MB.
 # Prepis metódu vypis_info tak, aby vypísala aj veľkosť súboru.
 
+class EKniha(Kniha):
+     def __init__(self,nazov,autor,rok,velkost_MB=0):
+          super().__init__(nazov,autor,rok)
+          self.velkost_MB = velkost_MB
+
+     def __str__(self):
+         return f"{super().__str__()}, {self.velkost_MB}MB"
+
+     def vypis_info(self):
+         return f"{self.nazov}, {self.autor}, {self.rok}, {self.velkost_MB}MB"
+
+ekniha = EKniha("Java I", "Java Guru",2008,25)
+
+print(ekniha)
+print(ekniha.vypis_info())
+     
+
 
 # ---------------------------------------------------------
 
@@ -38,3 +83,14 @@ x = [{"nazov" : "python guru I", "autor" : "Guru" , "rok" : 2008},
 # Zadanie:
 # Vytvor triedu StaraKniha, ktorá dedí z Kniha a má metódu je_historicka(),
 # ktorá vráti True ak bola vydaná pred rokom 1950.
+
+
+class StaraKniha(Kniha):
+
+    def je_historicka(self):
+        return self.rok < 1950
+
+sk = StaraKniha("1984","Orwell",1948)
+
+print(sk)
+print(sk.je_historicka())
