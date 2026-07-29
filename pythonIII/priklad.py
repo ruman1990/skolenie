9d5622518513a7953698ddc2e3ee45fa977ba7f7
Your Name <you@example.com>
2026-07-29 15:01:52 +0200

    pzthonIII

--- pythonIII/priklad.py
+++ pythonIII/priklad.py
@@ -1,14 +1,11 @@
-import csv
+import json
 
-# with open('names.csv', 'w',newline="") as f:
-#     fnames = ['first_name', 'last_name']
-#     writer = csv.DictWriter(f, fieldnames=fnames,delimiter="#")
-#     writer.writeheader()
-#     writer.writerow({'first_name' : 'John', 'last_name': 'Smith#Wesson'})
-#     writer.writerow({'first_name' : 'Robert', 'last_name': 'Brown'})
-#     writer.writerow({'first_name' : 'Julia', 'last_name': 'Griffin'})
+data = {
+    "meno": "Peter Š.",
+    "vek": 30,
+    "email": "peter@email.com",
+    "aktivny": False
+}
 
-with open("names.csv","r") as f:
-    reader = csv.reader(f,delimiter="#")
-    for x in reader:
-        print(x)
+with open('peter.json', 'w', encoding='utf-8') as f:
+    json.dump(data, f,indent=4,ensure_ascii=False)
