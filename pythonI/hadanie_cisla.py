ef0d91eeb17bae7f93b9cf9100fa833d268a4d6d
Tvoje Meno <tvoj@email.com>
2026-09-17 14:53:58 +0200

    pythonI

--- pythonI/hadanie_cisla.py
+++ pythonI/hadanie_cisla.py
@@ -4,21 +4,24 @@
 # ak hada nizsie cislo alebo vyssie tak mu poradime
 # tajne cislo bude ako literal
 import random
-
-
-tajne_cislo = random.randint(1,100)
+secret_number = random.randint(1,100)
 pokusy = 7
-
 while pokusy > 0:
-    number = int(input(f"Hadaj cislo od 1-100 (Mas {pokusy} pokusov): "))
-    if number == tajne_cislo:
-        print("Uhadol si")
-        break
-    elif number < tajne_cislo:
-        print("Hadaj vyssie")
+    number = input(f"Hadaj cislo od 1 do 100 (Pocet pokusov {pokusy}): ")
+    if not number.isdigit():
+        print("Zadal si zle cislo")
+        continue
     else:
-        print("Hadaj nizsie")
+        number = int(number)
     pokusy -= 1
+    if secret_number == number:
+        print("Uhadol si!")
+        break
+    elif pokusy == 0:
+        print(f"Prehral si! Tajne cislo bolo {secret_number}")
+    elif secret_number > number:
+        print("Hadaj vyssie")
+    elif secret_number < number:
+        print("Hadaj nizsie")
 
-if pokusy==0:
-    print("Prehral si")
+
