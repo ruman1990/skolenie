e442aa9481821d7bb045da029cbc9c5000e0bbc5
Tvoje Meno <tvoj@email.sk>
2026-09-25 11:45:34 +0200

    pythonII

---
+++ pythonII/priklad_prace_so_suborom.py
@@ -0,0 +1,44 @@
+class Person:
+   def __init__(self,name,age):
+      self.name = name.strip()
+      self.age = age.strip()
+
+   def __str__(self):
+      return f"{self.name},{self.age}"
+
+   def to_file(self,last=False):
+      if last:
+         return f"{self.name},{self.age}"
+      else:
+         return f"{self.name},{self.age}\n"
+
+zoznam = []
+
+# with open("vysledok.csv","r",encoding="utf-8") as f:
+#    for x in f:
+#       z = x.split(";")
+#       if z[0] != 'Vlado':
+#          zoznam.append([z[0].strip(),z[1].strip()])
+
+# print(zoznam)
+
+# with open("vysledok2.csv","w",encoding="utf-8") as f:
+#    for x in zoznam[:-1]:
+#       f.write(",".join(x)+"\n")
+#    f.write(",".join(zoznam[-1]))
+#       #f.write(f"{",".join(x)}\n")
+
+
+with open("vysledok.csv","r",encoding="utf-8") as f:
+   for x in f:
+      z = x.split(";")
+      if z[0] != 'Vlado':
+         zoznam.append(Person(*z))
+
+for x in zoznam:
+   print(x)
+
+with open("vysledok2.csv","w",encoding="utf-8") as f:
+   for x in zoznam[:-1]:
+      f.write(x.to_file())
+   f.write(zoznam[-1].to_file(True))
