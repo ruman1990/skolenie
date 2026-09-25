42a158bd09007a62ae10c8546c54391fdd9d36fb
Tvoje Meno <tvoj@email.sk>
2026-09-25 13:37:51 +0200

    pythonII

--- pythonII/priklad.py
+++ pythonII/priklad.py
@@ -1,15 +1,4 @@
 import csv
+#import priklad_prace_so_suborom as ps
+from priklad_prace_so_suborom import *
 
-zoznam = []
-
-with open("vysledok.csv","r",encoding="utf-8") as f:
-    reader = csv.reader(f,delimiter=";")
-    for x in reader:
-        print(x)
-        if x[0] != "Vlado":
-         zoznam.append(x)
-
-
-with open("vysledok2.csv","w",encoding="utf-8",newline="") as f:
-   writer = csv.writer(f,delimiter=",")
-   writer.writerows(zoznam)
--- pythonII/priklad_prace_so_suborom.py
+++ pythonII/priklad_prace_so_suborom.py
@@ -28,34 +28,39 @@ zoznam = []
 #    f.write(",".join(zoznam[-1]))
 #       #f.write(f"{",".join(x)}\n")
 
-
-with open("vysledok.csv","r",encoding="utf-8") as f:
-   for x in f:
-      z = x.split(";")
-      if z[0] != 'Vlado':
-         zoznam.append(Person(*z))
-
-for x in zoznam:
-   print(x)
-
-with open("vysledok2.csv","a",encoding="utf-8") as f:
-   for x in zoznam[:-1]:
-      f.write(x.to_file())
-   f.write(zoznam[-1].to_file(True))
-
-# riesenie cez CSV modul
-import csv
-
-zoznam = []
-
-with open("vysledok.csv","r",encoding="utf-8") as f:
-    reader = csv.reader(f,delimiter=";")
-    for x in reader:
+def main():
+    with open("vysledok.csv","r",encoding="utf-8") as f:
+        for x in f:
+            z = x.split(";")
+            if z[0] != 'Vlado':
+                zoznam.append(Person(*z))
+    
+    for x in zoznam:
         print(x)
-        if x[0] != "Vlado":
-         zoznam.append(x)
+
+    with open("vysledok2.csv","a",encoding="utf-8") as f:
+        for x in zoznam[:-1]:
+            f.write(x.to_file())
+        f.write(zoznam[-1].to_file(True))
+    
+    # riesenie cez CSV modul
+    import csv
+
+    zoznam = []
+
+    with open("vysledok.csv","r",encoding="utf-8") as f:
+        reader = csv.reader(f,delimiter=";")
+        for x in reader:
+            print(x)
+            if x[0] != "Vlado":
+                zoznam.append(x)
 
 
-with open("vysledok2.csv","w",encoding="utf-8",newline="") as f:
-   writer = csv.writer(f,delimiter=",")
-   writer.writerows(zoznam)
+    with open("vysledok2.csv","w",encoding="utf-8",newline="") as f:
+        writer = csv.writer(f,delimiter=",")
+        writer.writerows(zoznam)
+
+
+if __name__ == "__main__":
+    main()
+    
